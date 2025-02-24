2df41085d96b8640465f1f312319698e2d15aa92
warley farias <157419864+warrley@users.noreply.github.com>
2025-02-24 11:02:40 -0300

    header logo done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-
+import s from './App.module.css'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className={s.main}>
+      <header>
+        <div className={s.headerContainer}>
+          <h1>W</h1>
+          <h3>imc</h3>
+          <span>powered by warley</span>
+        </div>
+      </header>
+
+      <div className={s.container}>
+        <div className={s.leftSide}>
+          ...
+        </div>
+
+        <div className={s.rightSide}>
+          ...
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
